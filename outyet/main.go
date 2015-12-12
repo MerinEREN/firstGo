4ab84bcffdea9e1b2daabb49840b0d948ef816a5
MerinEREN <merineren@gmail.com>
2015-12-12 13:41:24 +0200

    feature_branch's second commit

--- outyet/main.go
+++ outyet/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 )
 
+var ffdf = 2134
+
 func main() {
 	// feature branch comment for commit
-	ffdf := 21312323
+	ffdf = 21312323
 	dsssds := 34354545
 	ffdf += dsssds
 	fmt.Println("out yet !!!!!!!!!!!!!!!!\n")
