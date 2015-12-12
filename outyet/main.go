42dd3bd5218eee5b0ee93f3ebebff4c1a16ac939
MerinEREN <merineren@gmail.com>
2015-12-12 17:47:33 +0200

    feature branch one commit

--- outyet/main.go
+++ outyet/main.go
@@ -12,8 +12,8 @@ func main() {
 	dsssds := 34354545
 	ffdf += dsssds
 	fmt.Println("out yet !!!!!!!!!!!!!!!!\n")
-	// 2 bug fix lines below
 	wrerr := 32433434
 	erwrerrewrwr := 32424343434
 	wrerr -= erwrerrewrwr
+	// feature branch one comment
 }
